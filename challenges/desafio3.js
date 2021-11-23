b6a8ca59a2deb8417012f05566067e4ec710d7ca
mariananbp <mariananbp@gmail.com>
2021-11-23 18:23:47 -0300

    Challenge 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,22 @@
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: NumberInt(0) } },
+  { upsert: true },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["bovino"] } },
+  { $inc: { avaliacao: 5 } },
+  { upsert: true },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["ave"] } },
+  { $inc: { avaliacao: 3 } },
+  { upsert: true },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, avaliacao: 1 },
+);
